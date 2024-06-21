cedbd3b76f8de3b5632d8ca0bcf124f3ca6d2e2f
Jovi Joshua Tania <jovijoshuatania027@gmail.com>
2024-06-21 08:57:11 +0800

    00000000

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,6 +1,6 @@
 <template>
-    <div style="display: flex; justify-content: center; background-color: blue;">
-        <div style="display: flex; justify-content: center; position: relative; width: 900px; height: 850px; background-color: red;">
+    <div style="display: flex; justify-content: center;">
+        <div style="display: flex; justify-content: center; position: relative; width: 900px; height: 850px; ">
 
             <iframe :src="defaultSource" class="iframe-pc" frameborder="0" width="1600" height="900" style="position: absolute; z-index: 2; right: -305px; bottom: 100px; "></iframe>
             <iframe :src="defaultSource" class="iframe-mobile" frameborder="0" width="330" height="610" style="position: absolute; z-index: 4; left: 115px; bottom: -160px;"></iframe>
